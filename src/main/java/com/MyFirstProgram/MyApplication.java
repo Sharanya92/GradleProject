4b363a114bbb25f6aec6612ea0e1431ec73d9c78
Sharanya <sharanya.chandran1@gmail.com>
2022-12-10 15:51:48 +0530

    Initial commit

---
+++ src/main/java/com/MyFirstProgram/MyApplication.java
@@ -0,0 +1,10 @@
+package com.MyFirstProgram;
+
+public class MyApplication {
+    public static void main(String args[])
+    {
+        //SpringApplication.run(Application.class,args);
+        System.out.println("hELLO");
+
+    }
+}
